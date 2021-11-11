1180f6f76ff4cf9fdcb8f9eb1c82e599822c4b7d
admin <admin@MY-PC>
2021-11-11 00:37:26 -0800

    push 1

---
+++ Github.practice/src/test/java/github/practice/test1.java
@@ -0,0 +1,14 @@
+package github.practice;
+
+import org.testng.annotations.Test;
+
+public class test1 
+{
+	@Test
+	
+	public void github() {
+		System.out.println("hi this is user 1");
+	}
+	
+
+}
